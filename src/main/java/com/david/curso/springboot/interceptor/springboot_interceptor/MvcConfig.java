ab2e3bfa3cbc0cfe9921761a9816fb66a19c4eb1
David MTZD <davidmtzd21512@gmail.com>
2024-09-30 17:31:29 -0600

    se creo el proyecto la clase loadtimeinterceptor y mvcConfig

---
+++ src/main/java/com/david/curso/springboot/interceptor/springboot_interceptor/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.david.curso.springboot.interceptor.springboot_interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private HandlerInterceptor timeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
+
+}
